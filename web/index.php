eb79502b650431855301a2a906fb055f2a801eda
Ben Gross <brgross@gmail.com>
2015-07-26 10:49:05 -0400

    Demo Times

--- web/index.php
+++ web/index.php
@@ -10,11 +10,22 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Reigster the Twig templating engine
+$app->register(new Silex/Provider/TwigServiceProvider(), array(
+	'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/twig/{name}', function ($name) user ($app) {
+	return $app['twig']->render('index.twig', array(
+		'name' => $name,
+	))
+});
+
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+  return str_repeat('Hello', getenv('TIMES'));
 });
 
 $app->run();
