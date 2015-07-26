f780699931d87eeda7487b4e7d7c736dc87c768a
Ben Gross <brgross@gmail.com>
2015-07-26 11:07:41 -0400

    fixing twig

--- web/index.php
+++ web/index.php
@@ -10,16 +10,25 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Reigster the Twig templating engine
-$app->register(new Silex/Provider/TwigServiceProvider(), array(
-	'twig.path' => __DIR__.'/../views',
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
 ));
 
-$app->get('/twig/{name}', function ($name) user ($app) {
-	return $app['twig']->render('index.twig', array(
-		'name' => $name,
-	))
+$app->get('/twig/{name}', function ($name) use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+    ));
 });
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
 
 // Our web handlers
 
@@ -27,6 +36,21 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return "Hello Friends";
   //return str_repeat('Hello', getenv('TIMES'));
+});
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
 });
 
 $app->run();
